20f158c598782ad0d3835669287ca5ea74479392
Jason Wadsworth <jdwadsworth@gmail.com>
2013-06-16 17:45:09 -0500

    Added solution for problem 8

---
+++ problem8/problem8.go
@@ -0,0 +1,43 @@
+package problem8
+
+// 8. Detecting ECB
+//
+// At the following URL are a bunch of hex-encoded ciphertexts:
+//
+//    https://gist.github.com/3132928
+//
+// One of them is ECB encrypted. Detect it.
+//
+// Remember that the problem with ECB is that it is stateless and
+// deterministic; the same 16 byte plaintext block will always produce
+// the same 16 byte ciphertext.
+
+import "bufio"
+import "subakva/matasano/utils"
+import "bytes"
+
+func HasRepeatedBlocks(decoded []byte, blockSize int) bool {
+  chunks := utils.ChunkBytes(decoded, blockSize)
+  for i := 0; i < len(chunks); i++ {
+    for j := 0; j < len(chunks); j++ {
+      if i == j { continue }
+      if bytes.Equal(chunks[i], chunks[j]) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+func ScanRepeatedBlocks(scanner *bufio.Scanner) string {
+  decoded := utils.DecodeBase64(scanner.Bytes())
+  if HasRepeatedBlocks(decoded, 16) {
+    return scanner.Text()
+  } else {
+    return ""
+  }
+}
+
+func DetectECB(filename string) string {
+  return utils.ReadAndScan(filename, ScanRepeatedBlocks)
+}
